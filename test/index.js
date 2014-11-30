73232a84d74f4922ea7c99b6dd8e50e796fd9e98
queckezz <fabian.eichenberger@gmail.com>
2014-11-30 17:21:49 +0100

    add test for #25

--- test/index.js
+++ test/index.js
@@ -57,6 +57,31 @@ describe('koa-views', function () {
       .expect(200, done)
   })
 
+  // #25
+  it('works with circular references in locals', function (done) {
+    var app = koa()
+    .use(views({ default: 'jade' }))
+    .use(function *() {
+      this.locals = {
+        a: {},
+        app: app
+      }
+
+      this.locals.a.a = this.locals.a
+
+      yield this.render('./fixtures/global-locals', {
+        app: app,
+        b: this.locals,
+        engine: 'jade'
+      })
+    })
+
+    request(app.listen()).get('/')
+      .expect('Content-Type', /html/)
+      .expect(/basic:jade/)
+      .expect(200, done)
+  })
+
   it('`map` given `engine` to given file `ext`', function (done) {
     var app = koa()
     .use(views({ map: {html: 'underscore'} }))
