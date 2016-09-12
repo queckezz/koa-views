f43468c14ffa3e22a3b1bc0b0ff556024f06317a
queckezz <fabian.eichenberger@gmail.com>
2016-09-12 13:36:33 +0200

    restructure functions

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 
 'use strict'
 
-/**
-* Module dependencies.
-*/
-
 const { resolve, dirname, extname, join } = require('path')
 const debug = require('debug')('koa-views')
 const consolidate = require('consolidate')
@@ -12,63 +8,9 @@ const defaults = require('@f/defaults')
 const { stat } = require('mz/fs')
 const send = require('koa-send')
 
-/**
-* Check if `ext` is html.
-* @return {Boolean}
-*/
+module.exports = viewsMiddleware
 
-const isHtml = (ext) => ext === 'html'
-
-/**
- * File formatter.
- */
-
-const toFile = (fileName, ext) => `${fileName}.${ext}`
-
-/**
- * Get the right path, respecting `index.[ext]`.
- * @param  {String} abs absolute path
- * @param  {String} rel relative path
- * @param  {String} ext File extension
- * @return {Object} tuple of { abs, rel }
- */
-
-function getPaths(abs, rel, ext) {
-  return stat(join(abs, rel)).then((stats) => {
-    if (stats.isDirectory()) {
-      // a directory
-      return {
-        rel: join(rel, toFile('index', ext)),
-        abs: join(abs, dirname(rel), rel)
-      }
-    }
-
-    // a file
-    return {
-      rel,
-      abs
-    }
-  })
-  .catch((e) => {
-    // not a valid file/directory
-    if (!extname(rel)) {
-      // Template file has been provided without extension
-      // so append to it to try another lookup
-      return getPaths(abs, `${rel}.${ext}`, ext)
-    }
-
-    throw e
-  })
-}
-
-/**
- * Add `render` method.
- *
- * @param {String} path
- * @param {Object} opts (optional)
- * @api public
- */
-module.exports = (path, opts) => {
+function viewsMiddleware (path, opts) {
   opts = defaults(opts || {}, {
     extension: 'html'
   })
@@ -78,14 +20,6 @@ module.exports = (path, opts) => {
   return function views (ctx, next) {
     if (ctx.render) return next()
 
-    /**
-     * Render `view` with `locals` and `koa.ctx.state`.
-     *
-     * @param {String} view
-     * @param {Object} locals
-     * @return {GeneratorFunction}
-     * @api public
-     */
     ctx.render = function (relPath, locals) {
       if (locals == null) {
         locals = {}
@@ -124,4 +58,40 @@ module.exports = (path, opts) => {
 
     return next()
   }
+}
+
+function getPaths(abs, rel, ext) {
+  return stat(join(abs, rel)).then((stats) => {
+    if (stats.isDirectory()) {
+      // a directory
+      return {
+        rel: join(rel, toFile('index', ext)),
+        abs: join(abs, dirname(rel), rel)
+      }
+    }
+
+    // a file
+    return {
+      rel,
+      abs
+    }
+  })
+  .catch((e) => {
+    // not a valid file/directory
+    if (!extname(rel)) {
+      // Template file has been provided without extension
+      // so append to it to try another lookup
+      return getPaths(abs, `${rel}.${ext}`, ext)
+    }
+
+    throw e
+  })
+}
+
+function isHtml (ext) {
+  return ext === 'html'
+}
+
+function toFile (fileName, ext) {
+  return `${fileName}.${ext}`
 }
